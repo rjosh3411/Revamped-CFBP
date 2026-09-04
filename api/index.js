44c9c4e0309913af0da89e7839056d0e10e398b3
rjosh3411 <rjosh3411@gmail.com>
2026-09-03 21:42:40 -0400

    Deploy hook order fix and clean Vite bundle

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const app = require('../server/index.js');
 
 module.exports = app;
-// Vercel redeploy Fri Sep  4 01:42:00 EDT 2026 - Fix React rules of hooks in LiveScoreboardRibbon
+// Vercel redeploy Fri Sep  4 01:43:00 EDT 2026 - Fresh client build and React Hook fix
 
